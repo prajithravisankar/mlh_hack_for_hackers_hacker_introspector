ee6e40f846b21da85cf5c1bc8effee104ea2ee70
prajithravisankar <prajithravisankar@gmail.com>
2026-01-02 19:54:54 -0500

    setup the server it is now running and listening

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,29 @@
-package server
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/prajithravisankar/mlh_hack_for_hackers_hacker_introspector/internal/introspect"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Hacker introspector api is live!!",
+		})
+	})
+
+	router.GET("/status", func(context *gin.Context) {
+		currentProject := introspect.Repository{
+			Name: "Hacker Introspector",
+		}
+
+		context.JSON(http.StatusOK, currentProject)
+	})
+
+	router.Run(":8080")
+}
